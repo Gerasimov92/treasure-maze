b8cb191cc0e03eeb61df34a743fed997a5b43ac3
Ivan Gerasimov <aka.montoya@gmail.com>
2021-05-24 15:50:41 +0300

    Add player hide feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private GameObject body;
+    [SerializeField] private GameObject camo;
 
     private Vector2 _moveVector;
-
-    void Start()
-    {
-        
-    }
+    private bool _isHidden;
 
     void Update()
     {
@@ -21,7 +19,18 @@ public class PlayerController : MonoBehaviour
 
     private void OnMove(InputValue value)
     {
+        if (_isHidden)
+            Hide(false);
+
         _moveVector = value.Get<Vector2>();
+    }
+
+    private void OnHide(InputValue value)
+    {
+        if (!value.isPressed)
+            return;
+
+        Hide(!_isHidden);
     }
 
     private void Move(Vector2 direction)
@@ -32,5 +41,12 @@ public class PlayerController : MonoBehaviour
         var scaledMoveSpeed = moveSpeed * Time.deltaTime;
         var move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.y);
         transform.position += move * scaledMoveSpeed;
+    }
+
+    private void Hide(bool value)
+    {
+        _isHidden = value;
+        body.SetActive(!value);
+        camo.SetActive(value);
     }
 }
